e33b283d312cedd01c2be4ada220ab5aa5140451
Raina <764883754@qq.com>
2016-08-15 21:20:28 +0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,42 @@
+;$(function(){
+	'use strict';
 
+	var $sidebar=$('#sidebar'),
+		$mask=$('.mask'),
+		$sidebar_trigger=$('#sidebar_trigger'),
+		$backButton=$('.backToTop');
+
+	
+	$sidebar_trigger.on('click',showSidebar);
+	$mask.on('click',hideSidebar);
+	$backButton.on('click',backToTop);
+
+	$(window).on('scroll',function(){
+		if ($(window).scrollTop()>$(window).height()) 
+		{
+			$backButton.fadeIn();
+		}
+		else{
+			$backButton.fadeOut();
+		}
+	})
+
+	$(window).trigger('scroll');
+
+	function showSidebar() {
+		
+			$mask.fadeIn();
+			$sidebar.css('right',0);
+	}
+	function hideSidebar() {
+		
+			$mask.fadeOut();
+			$sidebar.css('right',-$sidebar.width());
+	}
+	function backToTop() {
+		
+			$('html,body').animate({
+				scrollTop:0
+			},1000);
+	}
+})
